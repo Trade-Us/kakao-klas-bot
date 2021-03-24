32df46fd8f6aa81c9828455e8d6b584f3fff910f
shimshim485 <50603345+shimshim485@users.noreply.github.com>
2021-03-24 14:25:58 +0900

    Update crawling.py

--- server/crawling.py
+++ server/crawling.py
@@ -2,43 +2,6 @@ from database import init_db
 from database import db_session 
 from crawl_models import IDWithSubject, User, Subject, Assignment, Notice, OnlineLecture
 
-def add_IDWithSubject(crawling):
-    data=IDWithSubject(crawling[0], crawling[1])
-    db_session.add(data)
-    db_session.commit()
-def delete_IDWithSubject(userID, subjectID):
-
-
-def add_User(name, password):
-    data=User(name, password)
-    db_session.add(data)
-    db_session.commit()
-def delete_User(name, password):
-
-def add_Subject(name, professor,schedule):
-    data=Subject(name, professor,schedule)
-    db_session.add(data)
-    db_session.commit()
-def delete_Subject(name, professor,schedule):
-
-def add_Assignment(title, startDate, endDate, submit, subjectID):
-    data=Assignment(title, startDate, endDate, submit, subjectID)
-    db_session.add(data)
-    db_session.commit()
-def delete_Assignment(title, startDate, endDate, submit, subjectID):
-
-def add_Notice(title, writer,date, contents, serialNum, subjectID):
-    data=Notice(title, writer,date, contents, serialNum, subjectID)
-    db_session.add(data)
-    db_session.commit()
-def delete_Notice(title, writer,date, contents, serialNum, subjectID):
-
-def add_OnlineLecture(startDate, endDate, progress, contents, week, episode, subjectID):
-    data=OnlineLecture(startDate, endDate, progress, contents, week, episode, subjectID)
-    db_session.add(data)
-    db_session.commit()
-def delete_OnlineLecture(startDate, endDate, progress, contents, week, episode, subjectID):
-
 def main():
     add_IDWithSubject("2018203039", "sdh9606")
     add_IDWithSubject("2018203040", "sdh960")
