fb3b8a00e3543003d99665c94724f88cc51c61aa
Dahye Shim <shimshim485@naver.com>
2021-03-25 22:49:37 +0900

    Create read_db.py

---
+++ read_db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from server.models import User, IDWithSubject, Subject, Assignment,Notice, OnlineLecture
+
+engine = create_engine('sqlite:///information.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def read_User():
+    users = session.query(User).order_by(User.ID)
+    user_lists=[]
+    for user in users:
+        user_set=[user.ID, user.Name, user.Password]
+        user_lists.append(user_set)
+
+    return user_lists
+
+ 
