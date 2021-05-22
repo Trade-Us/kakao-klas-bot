40aaaf0de570d65d98de5554c701abc9ed6be6b8
MoSangIl_mac <mosang1230@gmail.com>
2021-05-23 01:56:20 +0900

    REVISE register.py : exception for registering two ids in one bot, and trying to register while scraping new user's info

--- register.py
+++ register.py
@@ -1,37 +1,61 @@
 from crawlingDriver import MyThreadDriver
-from database import init_db 
-from database import db_session 
+from database import init_db
+from database import db_session
 from server.models import User, Subject, IDWithSubject, NewUser
 import time
 from bs4 import BeautifulSoup
 from crypto_function import SymmetricKeyAgent
 
-def register_user(parm_id,name,parm_password, kakaoid):
+
+def register_user(parm_id, name, parm_password, kakaoid):
     # 카카오톡 한 개 user 확인
-    # check_oneId_oneBot = db_session.query(User).filter_by(UserKey=kakaoid).first()
-    # if check_oneId_oneBot:
-    #   #카카오톡 2개 이상 id를 등록 하려 하는 경우
-    #     dataSend = {
-    #         "version": "2.0",
-    #         "template": {
-    #             "outputs": [
-    #                 {
-    #                     "carousel": {
-    #                         "type" : "basicCard",
-    #                         "items": [
-    #                             {
-    #                                 "title" : "봇에서는 하나의 아이디만 등록 가능합니다.",
-    #                                 "description" : "ㅎㅎ"
-    #                             }
-    #                         ]
-    #                     }
-    #                 }
-    #             ]
-    #         }
-    #     }
-    #     return -1, dataSend
-    
+    check_oneId_oneBot = db_session.query(
+        User).filter_by(UserKey=kakaoid).first()
+    if check_oneId_oneBot:
+      # 카카오톡 2개 이상 id를 등록 하려 하는 경우
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": "하나의 아이디만 등록 가능합니다.",
+                                    "description": "다른 아이디를 등록하기 원하신다면, 등록해지후 로그인을 시도해주세요 :)"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+        return -1, dataSend
+
     # User 중복 확인
+    holdon_user = db_session.query(NewUser).filter_by(ID=parm_id).first()
+    if holdon_user:
+        # 로그인 실패한 경우
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                        {
+                            "carousel": {
+                                "type": "basicCard",
+                                "items": [
+                                    {
+                                        "title": "이미 등록된 유저입니다.",
+                                        "description": "현재 사용자의 정보를 읽고 있는 중입니다. 잠시만 기다려주세요^^"
+                                    }
+                                ]
+                            }
+                        }
+                ]
+            }
+        }
+        return -1, dataSend
     user = db_session.query(User).filter_by(ID=parm_id).first()
     if not user:
         # 로그인 시도 (일단 무조건 성공)
@@ -41,7 +65,8 @@ def register_user(parm_id,name,parm_password, kakaoid):
         # 성공시 register
         time.sleep(1)
         soup = BeautifulSoup(myThreadDriver.driver.page_source, 'html.parser')
-        subjects = soup.select("#appModule > div > div:nth-child(1) > div:nth-child(2) > ul > li")
+        subjects = soup.select(
+            "#appModule > div > div:nth-child(1) > div:nth-child(2) > ul > li")
         # 실패시 succeed = false
         if not subjects:
             # 로그인 실패한 경우
@@ -51,11 +76,11 @@ def register_user(parm_id,name,parm_password, kakaoid):
                     "outputs": [
                         {
                             "carousel": {
-                                "type" : "basicCard",
+                                "type": "basicCard",
                                 "items": [
                                     {
-                                        "title" : "로그인 실패!",
-                                        "description" : "ID PW 확인해주세요.."
+                                        "title": "로그인 실패!",
+                                        "description": "ID PW 확인해주세요.."
                                     }
                                 ]
                             }
@@ -67,7 +92,8 @@ def register_user(parm_id,name,parm_password, kakaoid):
         # 새 유저인 경우, New_User에 등록..
         keyAgent = SymmetricKeyAgent()
         cipher_pw = keyAgent.encrypt(parm_password)
-        new_user = NewUser(ID=parm_id, Name="모상일", Password=cipher_pw, UserKey=kakaoid)
+        new_user = NewUser(ID=parm_id, Name="모상일",
+                           Password=cipher_pw, UserKey=kakaoid)
         db_session.add(new_user)
 
         # print(subjects)
@@ -82,12 +108,14 @@ def register_user(parm_id,name,parm_password, kakaoid):
         for subject in result:
             sub = db_session.query(Subject).filter_by(ID=subject[1]).first()
             if not sub:
-                s = Subject(ID=subject[1],Name=subject[0],Professor=subject[2],Schedule="")
+                s = Subject(ID=subject[1], Name=subject[0],
+                            Professor=subject[2], Schedule="")
                 db_session.add(s)
-                            
-            id_sub_set = db_session.query(IDWithSubject).filter_by(UserID=parm_id,SubjectID=subject[1]).first()
+
+            id_sub_set = db_session.query(IDWithSubject).filter_by(
+                UserID=parm_id, SubjectID=subject[1]).first()
             if not id_sub_set:
-                id_set = IDWithSubject(UserID=parm_id,SubjectID=subject[1])
+                id_set = IDWithSubject(UserID=parm_id, SubjectID=subject[1])
                 db_session.add(id_set)
         db_session.commit()
         db_session.close()
@@ -103,11 +131,11 @@ def register_user(parm_id,name,parm_password, kakaoid):
                 "outputs": [
                     {
                         "carousel": {
-                            "type" : "basicCard",
+                            "type": "basicCard",
                             "items": [
                                 {
-                                    "title" : "이미 등록된 사용자입니다.",
-                                    "description" : "ㅎㅎ"
+                                    "title": "이미 등록된 사용자입니다.",
+                                    "description": "ㅎㅎ"
                                 }
                             ]
                         }
@@ -116,8 +144,7 @@ def register_user(parm_id,name,parm_password, kakaoid):
             }
         }
         return -1, dataSend
-    
-    
+
     # 실패시 오류 처리
 _, v = register_user("2018203092", "df", "tkddlf^^12", "sdf")
 print(v)
