8a9e4ef8789a25e349784a4516afc795e417e3f0
SangIl-fnino <mosang1230@naver.com>
2021-06-10 15:15:11 +0900

    use headless

--- crawlingDriver.py
+++ crawlingDriver.py
@@ -16,7 +16,7 @@ class MyThreadDriver(threading.Thread):
     def __init__(self, _id, _pw, crawling_page_function):
         threading.Thread.__init__(self)
         options = webdriver.ChromeOptions()
-        # options.add_argument("headless")
+        options.add_argument("headless")
         options.add_argument("disable-gpu")
         # options.add_argument("disable-infobars")
         options.add_argument("no-sandbox")
